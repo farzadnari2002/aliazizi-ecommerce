75dd24fa8703fd3bb1a23c175fdcd33e645a2123
aliazizi-code <devaliazizi@gmail.com>
2025-01-25 16:19:29 +0330

    Add custom User and UserProfile models
    - Implemented a custom User model with email and number as identifiers.
    - Created UserProfile model for additional user information.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation.trans_null import gettext_lazy as _
 
-# Create your models here.
+from managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=True, null=True)
+    number = models.CharField(max_length=12, unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    permissions = models.CharField(max_length=255, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'number'
+    REQUIRED_FIELDS = ['email']
+
+
+class UserProfile(models.Model):
+    class Gender(models.TextChoices):
+        male = 'M', _('Male')
+        female = 'F', _('Female')
+        other = 'O', _('Other')
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(max_length=1, choices=Gender.choices, default=Gender.other)
