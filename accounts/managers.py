8e229f205a4f6b14a559885a236febffc1e536d0
aliazizi-code <devaliazizi@gmail.com>
2025-01-30 15:08:45 +0330

    Implement create_superuser method
    - Added create_superuser method to create an admin user with phone number or email.
    - Ensured superuser attributes are set: is_admin, is_superuser, is_staff, and is_active.
    - Includes error handling for missing phone number and email.

--- accounts/managers.py
+++ accounts/managers.py
@@ -10,30 +10,33 @@ class UserManager(BaseUserManager):
         if not number and not email:
             raise ValueError("Users must have an phone number or a email address")
 
-        if email:
-            email = self.normalize_email(email)
         if number:
             number = number.strip()
 
         user = self.model(
             number=number if number else None,
-            email=email if email else None
+            email=self.normalize_email(email) if email else None
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, number=None, password=None):
+    def create_superuser(self, number=None, email=None, password=None):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
         """
+        if not number and not email:
+            raise ValueError("Superuser must have a phone number or an email address.")
+
         user = self.create_user(
-            number,
+            number=number,
+            email=email,
             password=password,
         )
         user.is_admin = True
+        user.is_superuser = True
         user.is_staff = True
         user.is_active = True
         user.save(using=self._db)
