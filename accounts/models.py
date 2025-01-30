18daa960c26402ebad8db2f6e570cba24dd96088
aliazizi-code <devaliazizi@gmail.com>
2025-01-30 15:06:51 +0330

    Implement User model enhancements
    - Added PhoneNumberField for unique phone number management.
    - Introduced `is_admin` field for admin status.
    - Implemented `full_name` method for formatted name.
    - Enhanced string representation with phone number or email.
    - Defined permission methods: has_perm and has_module_perms.
    - Added is_staff property to identify admin users.

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation.trans_null import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
-from managers import UserManager
+from .managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, blank=True, null=True)
-    number = models.CharField(max_length=12, unique=True)
+    number = PhoneNumberField(unique=True, region='IR')
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
 
     is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     permissions = models.CharField(max_length=255, default='')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,6 +23,31 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'number'
     REQUIRED_FIELDS = ['email']
+
+    def full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
+        return ""
+
+    def __str__(self):
+        full_name = self.full_name()
+        return f"{full_name.strip()} ({self.number or self.email})".strip()
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
 
 class UserProfile(models.Model):
