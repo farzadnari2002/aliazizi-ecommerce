61b55dfd9548d026657904217a973ef46b224e43
aliazizi-code <devaliazizi@gmail.com>
2025-02-02 11:52:43 +0330

    fix: Correct a minor mistake in the code

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,7 +11,7 @@ class PhoneNumberField(serializers.CharField):
     ]
 
 
-class RequestOTP(serializers.ModelSerializer):
+class RequestOTP(serializers.Serializer):
     number = PhoneNumberField(max_length=12)
 
 
