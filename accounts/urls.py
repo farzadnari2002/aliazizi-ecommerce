c1a05bb500f7d011d1db84ab44b3ab28836e4a42
aliazizi-code <devaliazizi@gmail.com>
2025-02-02 11:56:50 +0330

    chore: Add URL routes for OTP and email login functionality
    - Added 'request/' route for OTP generation.
    - Added 'verify-otp/' route for OTP verification.
    - Added 'email-login/' route for email-based login.
    - Included 'refresh/' route for JWT token refresh.

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from . import views
+
+urlpatterns = [
+    path('request/', views.GenerateOTPView.as_view(), name='request-otp'),
+    path('verify-otp/', views.VerifyOTPView.as_view(), name='verify-otp'),
+    path('email-login/', views.EmailLoginView.as_view(), name='email-login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
