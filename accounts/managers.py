fa285fa48337a3b8cf07785996664dcd7cdc9c66
aliazizi-code <devaliazizi@gmail.com>
2025-02-07 12:45:40 +0330

    delete user.is_staff in create_superuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -37,7 +37,6 @@ class UserManager(BaseUserManager):
         )
         user.is_admin = True
         user.is_superuser = True
-        user.is_staff = True
         user.is_active = True
         user.save(using=self._db)
         return user
