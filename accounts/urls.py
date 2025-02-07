09303209ec1575709b6da2790033c4e8830faea5
aliazizi-code <devaliazizi@gmail.com>
2025-02-07 16:58:01 +0330

    Add URL paths for user profile management and authentication
    - Added path for user profile update view.
    - Implemented path for setting new password.
    - Created path for setting user email.
    - Added path for changing user phone number.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('verify-otp/', views.VerifyOTPView.as_view(), name='verify-otp'),
     path('email-login/', views.EmailLoginView.as_view(), name='email-login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('user-profile/', views.UserProfileView.as_view(), name='user-profile'),
-
+    path('user-profile/', views.UpdateUserProfileView.as_view(), name='user-profile'),
+    path('set-new-password/', views.SetNewPasswordView.as_view(), name='set-new-password'),
+    path('set-email/', views.SetEmailView.as_view(), name='set-email'),
+    path('change-number/', views.ChangeNumberView.as_view(), name='change-number'),
 ]
