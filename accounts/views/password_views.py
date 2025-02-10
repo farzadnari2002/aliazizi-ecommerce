2e99dbb57fa8c2ac2fe1a30380231ecd7cc42489
aliazizi-code <devaliazizi@gmail.com>
2025-02-10 16:49:39 +0330

    Rename password views for clarity and enhance OTP verification logic for email and phone number resets

--- accounts/views/password_views.py
+++ accounts/views/password_views.py
@@ -1,38 +1,38 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
 from accounts.serializers.password_serializers import *
 from accounts.otp import *
+from accounts.models import User
 
 
 
-class SetNewPasswordRequestView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    serializer_class = SetNewPasswordRequestSerializer
+class ForgotPasswordRequestView(APIView):
+    serializer_class = ForgotPasswordRequestSerializer
 
     def post(self, request):
-        user=request.user
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             data = serializer.validated_data
-            new_number = data.get('number')
-            new_email = data.get('email')
+            number = data.get('number')
+            email = data.get('email')
 
-            if new_number:
-
-                if user.number != data['number']:
-                    otp = generate_otp_pass
+            if number:
+                user = get_object_or_404(User, number=number)
+                if user.number == data['number']:
+                    otp = generate_otp_pass(user.id)
                     print(f'Your OTP is: {otp}')
 
                     return Response({"detail": "OTP sent successfully."}, status=status.HTTP_200_OK)
                 return Response({"detail": "New number must be different from the current number."}, status=status.HTTP_400_BAD_REQUEST)
             
-            if new_email:
-                
+            if email:
+                user = get_object_or_404(User, email=email)
                 if user.email != data['email']:
-                    otp = generate_otp_pass
+                    otp = generate_otp_pass(user.id)
                     print(f'Your OTP is: {otp}')
                     #todo : send email
 
@@ -42,32 +42,45 @@ class SetNewPasswordRequestView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class SetNewPasswordVerifyView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-    serializer_class = SetNewPasswordVerifySerializer
+class ForgotPasswordVerifyView(APIView):
+    serializer_class = ForgotPasswordVerifySerializer
 
     def post(self, request):
-        user=request.user
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             data = serializer.validated_data
-            new_password = data['new_password']
+            number = data.get('number')
+            email = data.get('email')
             otp = data['otp']
+            new_password = data['password']
 
-            if verify_otp_pass(user.id, otp):
-                user.set_password(new_password)
-                user.save()
-                delete_otp_pass(user.id)
-                return Response({"detail": "Password changed successfully."}, status=status.HTTP_200_OK)
-            return Response({"detail": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
-
+            if number:
+                user = get_object_or_404(User, number=number)
+                if user.number == data['number']:
+                    if verify_otp_pass(user.id, otp):
+                        user.set_password(new_password)
+                        user.save()
+                        return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
+                    return Response({"detail": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"detail": "New number must be different from the current number."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            if email:
+                user = get_object_or_404(User, email=email)
+                if user.email != data['email']:
+                    if verify_otp_pass(user.id, otp):
+                        user.set_password(new_password)
+                        user.save()
+                        return Response({"detail": "Password reset successfully."}, status=status.HTTP_200_OK)
+                    return Response({"detail": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"detail": "New email must be different from the current email."}, status=status.HTTP_400_BAD_REQUEST)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChangePasswordView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = serializers.Serializer
+    serializer_class = ChangePasswordSerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
@@ -76,7 +89,7 @@ class ChangePasswordView(APIView):
         if serializer.is_valid():
             data = serializer.validated_data
             old_password = data['old_password']
-            new_password = data['new_password']
+            new_password = data['password']
 
             if user.check_password(old_password):
                 user.set_password(new_password)
