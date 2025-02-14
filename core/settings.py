63a63c52c584b6a27a8356519e2961b774bd1596
aliazizi-code <devaliazizi@gmail.com>
2025-02-14 12:32:41 +0330

    Add products app to installed apps in settings

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
+    'products.apps.ProductsConfig',
 
     'rest_framework',
     'rest_framework_simplejwt',
