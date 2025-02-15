b9251a8167445682fe001f220ab08e40fb627ba6
aliazizi-code <devaliazizi@gmail.com>
2025-02-15 16:05:47 +0330

    Register product-related models in the admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Product)
+admin.site.register(models.CategoryProduct)
+admin.site.register(models.ColorProduct)
+admin.site.register(models.ImagesProduct)
+admin.site.register(models.SpecificationsProduct)
+admin.site.register(models.CommentProduct)
