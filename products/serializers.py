356a69722e612f6e135cf37009f97f5cc273569d
aliazizi-code <devaliazizi@gmail.com>
2025-02-18 00:20:10 +0330

    Refactor ProductsListSerializer to remove tags field and simplify data representation

--- products/serializers.py
+++ products/serializers.py
@@ -1,14 +1,10 @@
 from rest_framework import serializers
 from .models import Product
-from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 
 
-class ProductsListSerializer(TaggitSerializer, serializers.ModelSerializer):
+class ProductsListSerializer(serializers.ModelSerializer):
     image_thumbnail = serializers.ImageField(read_only=True)
-    tags = TagListSerializerField()
 
     class Meta:
         model = Product
-        fields = ('name', 'slug', 'tags', 'sku', 'price',
-                  'image_thumbnail', 'created_at', 'updated_at', 'is_published', 'is_delete', 'avg_rating', 'category',) 
-        
+        fields = ('name', 'slug', 'price', 'image_thumbnail', 'avg_rating') 
