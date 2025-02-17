2357ae71259d1fdbf56f861a5c882f03dda3a04f
aliazizi-code <devaliazizi@gmail.com>
2025-02-17 18:07:47 +0330

    Add ProductsListView to handle product listing with published filter

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from products.serializers import ProductsListSerializer
+from products.models import Product  
 
-# Create your views here.
+
+class ProductsListView(APIView):
+    serializer_class = ProductsListSerializer
+
+    def get(self, request):
+        products = Product.objects.filter(is_published=True, is_delete=False, id=1)
+        serializer = self.serializer_class(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
