7349e5ceb381eadca3d51e848800a21c1443b99e
aliazizi-code <devaliazizi@gmail.com>
2025-02-17 14:46:11 +0330

    Register CategoryProduct with DraggableMPTTAdmin for improved admin interface

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 from . import models
 
 
 admin.site.register(models.Product)
-admin.site.register(models.CategoryProduct)
 admin.site.register(models.ColorProduct)
+admin.site.register(models.SizeProduct)
 admin.site.register(models.ImagesProduct)
 admin.site.register(models.SpecificationsProduct)
 admin.site.register(models.CommentProduct)
+admin.site.register(
+    models.CategoryProduct,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
