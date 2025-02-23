49274a7152625d0b9f4f89bbd7ade27cd77351c2
aliazizi-code <devaliazizi@gmail.com>
2025-02-23 18:55:48 +0330

    Add ProductDetailView to handle product detail retrieval and update ProductsListView to filter published products

--- products/views.py
+++ products/views.py
@@ -1,14 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from products.serializers import ProductsListSerializer
+from products.serializers import ProductsListSerializer, ProductDetailSerializer
 from products.models import Product  
+from django.shortcuts import get_list_or_404
 
 
 class ProductsListView(APIView):
     serializer_class = ProductsListSerializer
 
     def get(self, request):
-        products = Product.objects.filter(is_published=True, is_delete=False, id=1)
+        products = Product.objects.filter(is_published=True, is_delete=False)
         serializer = self.serializer_class(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductDetailView(APIView):
+    serializer_class = ProductDetailSerializer
+
+    def get(self, request, slug):
+        product = get_list_or_404(Product, slug=slug, is_published=True, is_delete=False)
+        serializer = self.serializer_class(product, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
