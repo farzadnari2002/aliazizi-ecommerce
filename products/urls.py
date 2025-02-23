6fb24f8185c5dac7e3ead78edbcf81ad7ed5becc
aliazizi-code <devaliazizi@gmail.com>
2025-02-23 18:55:54 +0330

    Add URL pattern for ProductDetailView to enable product detail retrieval

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from products import views
 
 urlpatterns = [
     path('list/', views.ProductsListView.as_view(), name='product-list'),
+    path('detail/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
