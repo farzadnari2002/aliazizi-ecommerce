927d67ab6cbee8066ef5dd0bf0c8be7032f99cb3
aliazizi-code <devaliazizi@gmail.com>
2025-02-25 22:54:24 +0330

    Add CategoriesListView to retrieve all categories and update imports accordingly

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from products.serializers import ProductsListSerializer, ProductDetailSerializer
-from products.models import Product  
+from products.serializers import ProductsListSerializer, ProductDetailSerializer, CategorySerializer
+from products.models import Product, CategoryProduct
 from django.shortcuts import get_list_or_404
 
 
@@ -21,4 +21,14 @@ class ProductDetailView(APIView):
     def get(self, request, slug):
         product = get_list_or_404(Product, slug=slug, is_published=True, is_delete=False)
         serializer = self.serializer_class(product, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CategoriesListView(APIView):
+    serializer_class = CategorySerializer
+
+    def get(self, request):
+        category = CategoryProduct.objects.all()
+        serializer = self.serializer_class(category, many=True)
+
         return Response(serializer.data, status=status.HTTP_200_OK)
