e798bdc59fb994e962e801f53320833f787ce66c
aliazizi-code <devaliazizi@gmail.com>
2025-02-28 17:47:54 +0330

    Update URL routing to replace LikeProductView with FavoriteProductView

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('list/', views.ProductsListView.as_view(), name='product-list'),
     path('detail/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('category/', views.CategoriesListView.as_view(), name='category'),
-    path('like/<slug:product_slug>/', views.LikeProductView.as_view({'post': 'like_product'}), name='like-product'),
+    path('like/', views.FavoriteProductView.as_view(), name='like'),
 ]
