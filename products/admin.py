4d752a9863c746b4efcea240653804e45a21738b
aliazizi-code <devaliazizi@gmail.com>
2025-02-28 17:47:08 +0330

    Update admin registration to use FavoriteProduct instead of LikeProduct

--- products/admin.py
+++ products/admin.py
@@ -4,7 +4,7 @@ from . import models
 
 
 admin.site.register(models.Product)
-admin.site.register(models.LikeProduct)
+admin.site.register(models.FavoriteProduct)
 admin.site.register(models.ColorProduct)
 admin.site.register(models.SizeProduct)
 admin.site.register(models.ImagesProduct)
