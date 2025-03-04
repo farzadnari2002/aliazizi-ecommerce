f8817aba9db75b6af93faba47b632dd76b768f3b
aliazizi-code <devaliazizi@gmail.com>
2025-03-04 12:10:05 +0330

    Add signals to manage favorites and comment counts for products

---
+++ products/signals.py
@@ -0,0 +1,30 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import FavoriteProduct, CommentProduct
+
+
+# Favorites Logic
+@receiver(post_save, sender=FavoriteProduct)
+def increment_favorites_count(sender, instance, **kwargs):
+    instance.product.favorites_count += 1
+    instance.product.save()
+
+@receiver(post_delete, sender=FavoriteProduct)
+def decrement_favorites_count(sender, instance, **kwargs):
+    instance.product.favorites_count -= 1
+    instance.product.save()
+
+
+# Comments Logic
+@receiver(post_save, sender=CommentProduct)
+def increment_comment_count(sender, instance, **kwargs):
+    instance.product.comment_count += 1
+    instance.product.save()
+
+@receiver(post_delete, sender=CommentProduct)
+def decrement_comment_count(sender, instance, **kwargs):
+    instance.product.comment_count -= 1
+    instance.product.save()
+
+
+
