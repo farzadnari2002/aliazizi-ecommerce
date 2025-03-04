fc4edd47fdf820f9d05190bca34359515e337a20
aliazizi-code <devaliazizi@gmail.com>
2025-03-04 15:46:39 +0330

    Add vote-comment URL path for managing comment votes

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('detail/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('category/', views.CategoriesListView.as_view(), name='category'),
     path('like/', views.FavoriteProductView.as_view(), name='like'),
+    path('vote-comment/', views.VoteCommentView.as_view(), name='vote-comment'),
 ]
