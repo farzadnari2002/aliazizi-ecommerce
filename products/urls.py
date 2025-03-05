6931d07e6053e982054becb88afffb20531ba6b4
aliazizi-code <devaliazizi@gmail.com>
2025-03-05 17:44:05 +0330

    Add URL pattern for ProductCommentsView to fetch comments by product slug

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('category/', views.CategoriesListView.as_view(), name='category'),
     path('like/', views.FavoriteProductView.as_view(), name='like'),
     path('vote-comment/', views.VoteCommentView.as_view(), name='vote-comment'),
+    path('comments/<slug:slug>/', views.ProductCommentsView.as_view(), name='comments'),
 ]
