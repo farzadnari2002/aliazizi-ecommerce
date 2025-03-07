be9aaf6e5816cd8fe635f585db6746eee09d46b6
aliazizi-code <devaliazizi@gmail.com>
2025-03-07 15:07:04 +0330

    Enhance comment handling by updating average rating and comment count in signals

--- products/signals.py
+++ products/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import FavoriteProduct, CommentProduct
+from .models import FavoriteProduct, CommentProduct, Product
+from django.db.models import Avg
 
 
 # Favorites Logic
@@ -17,13 +18,20 @@ def decrement_favorites_count(sender, instance, **kwargs):
 
 # Comments Logic
 @receiver(post_save, sender=CommentProduct)
-def increment_comment_count(sender, instance, **kwargs):
-    instance.product.comments_count += 1
-    instance.product.save()
+def increment_comment_count(sender, instance, created, **kwargs):
+    if created:
+        avg_rating = CommentProduct.objects.filter(product=instance.product).aggregate(Avg('rating'))['rating__avg']
+        
+        
+        instance.product.avg_rating = round(avg_rating, 1) if avg_rating is not None else 0
+        instance.product.comments_count += 1
+        instance.product.save()
 
 @receiver(post_delete, sender=CommentProduct)
 def decrement_comment_count(sender, instance, **kwargs):
     instance.product.comments_count -= 1
+    avg_rating = CommentProduct.objects.filter(product=instance.product).aggregate(Avg('rating'))['rating__avg']
+    instance.product.avg_rating = round(avg_rating, 1) if avg_rating is not None else 0
     instance.product.save()
 
 
