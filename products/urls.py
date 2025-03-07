1999f5a5e8013aee898efb11621aeee099fe0f0c
aliazizi-code <devaliazizi@gmail.com>
2025-03-07 15:07:49 +0330

    Update product list URL to use ProductsListViewSet for improved routing

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from products import views
 
 
 urlpatterns = [
-    path('list/', views.ProductsListView.as_view(), name='product-list'),
+    path('', views.ProductsListViewSet.as_view({'get': 'list'}), name='product-list'),
     path('detail/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('category/', views.CategoriesListView.as_view(), name='category'),
     path('like/', views.FavoriteProductView.as_view(), name='like'),
